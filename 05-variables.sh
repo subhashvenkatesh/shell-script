991b4c2237d0050c3844cd6d970c74456e82ba52
yamaha <venkateshsubhash.sv@gmail.com>
2024-04-11 16:12:19 +0530

    added 2nd script

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,4 +4,8 @@ echo "Please enter your username"
 
 read -s USERNAME
 
-echo "username is: $USERNAME"
+echo "username is: $USERNAME" # I am printing just for validation, you should not print
+
+echo "Please enter your password"
+
+read -s PASSWORD
