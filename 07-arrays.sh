e207be108336f4e7967818c9f894741bc843aef3
yamaha <venkateshsubhash.sv@gmail.com>
2024-04-15 12:51:11 +0530

    added 2nd script

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -4,7 +4,7 @@
 
 FRUITS=("APPLE" "BANANA" "MANGO")
 
-echo "first fruit is: $FRUITS[0]"
-echo "second fruit is: $FRUITS[1]"
-echo "third fruit is: $FRUITS[2]"
-echo "All values : $FRUITS[@]"
+echo "first fruit is: ${FRUITS[0]}"
+echo "second fruit is: ${FRUITS[1]}"
+echo "third fruit is: ${FRUITS[2]}"
+echo "All values : ${FRUITS[@]}"
