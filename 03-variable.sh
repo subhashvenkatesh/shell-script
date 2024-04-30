50863e2bd3f980d5b2c1b2f5ab49c2c5ce97d746
yamaha <venkateshsubhash.sv@gmail.com>
2024-04-30 11:59:37 +0530

    added 3rd script

--- 03-variable.sh
+++ 03-variable.sh
@@ -2,8 +2,10 @@
 
 PERSON1=$1
 PERSON2=$2
+PERSON3=$3
 
 echo "$PERSON1: Hello $PERSON2, Good Morning"
 echo "$PERSON2: Hi $PERSON1, Very Good Morning"
 echo "$PERSON1: How are you $PERSON2"
 echo "$PERSON2: I am good $PERSON1, how are you"
+echo "$PERSON3: Hellw my name is $PERSON3, so your $PERSON2 right"
