1e87e46975e783bca9240212af37d7763d2c1fa7
yamaha <venkateshsubhash.sv@gmail.com>
2024-05-05 00:11:33 +0530

    installing Multiple packages

---
+++ bull-scrit/03-samable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NUMBER1=$1
+NUMBER2=$2
+NUMBER3=$3
+
+SUBRACTION= ((NUMBER1-NUMBER2+NUMBER3))
+
+echo "total value = $SUBRACTION"
+
+echo "no args passed : $@"
+
+echo "name of the script = $0"
+
+echo "all args passed = $#"
