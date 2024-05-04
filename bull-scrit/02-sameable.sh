98d6e449e4c1ad9ecd46d3b2c5326811f2d339fc
yamaha <venkateshsubhash.sv@gmail.com>
2024-05-04 23:54:44 +0530

    installing Multiple packages

---
+++ bull-scrit/02-sameable.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PERSON1=Kamal
+PERSON2=Girish
+PERSON3=Hayathi
+PERSON4=Shruthi
+
+$PERSON1: HI $PERSON2 how are you 
+$PERSON2: yeah fine $PERSON1 what about you where is $PERSON4
+$PERSON1: $PERSON4 is on the way 
+$PERSON1: all set for party?
+$PERSON2: yeah waiting for my $PERSON3
+$PERSON1: i think $PERSON4 gnna be full at today
+$PERSON2: Yeah!!!!!
