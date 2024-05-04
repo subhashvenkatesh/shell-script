fcc815f92ba12ba8ae44ccd400be987e257df9fa
yamaha <venkateshsubhash.sv@gmail.com>
2024-05-04 11:48:32 +0530

    installing MySql through shell

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -1,42 +1,13 @@
 #!/bin/bash
 
-ID=$(id -u)
+ID=(id -u)
 
-if [ $ID -ne 0 ]
+if [ ID -ne 0 ]
+
 then
-   echo "ERROR: Please run this script with root access"
+   
+   echo "ERROR: Try with root user"
    exit 1
 else
-   echo "you are root user"
-
-fi
-
-yum install mysql -y
-
-if [ $? -ne 0 ]
-then
-   echo "ERROR : Installing MySQL is failed"
-   exit 1
-else
-    echo "MySQL installed successfully"
-fi
-
-yum install git -y
-
-if [ $? -ne 0 ]
-then
-    echo "ERROR: Installing Git is failed"
-    exit 1
-else
-    echo "Git is installed successfully"
-fi
-
-yum install java
-
-if [ $? -ne 0 ]
-
-then
-   echo "ERROR: installing Java is failed"
-else
-    echo "Java installed successfully"
-fi
+    echo "your a root user"
+fi    
