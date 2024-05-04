80741276cc062d60bb2ff2ca61084b225dd5a10b
yamaha <venkateshsubhash.sv@gmail.com>
2024-05-04 15:24:20 +0530

    installing MySql through shell

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ID=(id -u)
+ID=$(id -u)
 
 R="\e[31m"
 G="\e[32m"
@@ -8,6 +8,7 @@ Y="\e[33m"
 N="\e[0m"
 
 if [ $ID -ne 0 ]
+
 then
     echo -e "$R ERROR:$N $Y Please run this script with root access $N"
     exit 1
