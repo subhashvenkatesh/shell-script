05614fd1bc245e7f1e23097e6270b0bb864c640f
yamaha <venkateshsubhash.sv@gmail.com>
2024-05-04 23:57:14 +0530

    installing Multiple packages

--- bull-scrit/02-sameable.sh
+++ bull-scrit/02-sameable.sh
@@ -5,10 +5,10 @@ PERSON2=Girish
 PERSON3=Hayathi
 PERSON4=Shruthi
 
-$PERSON1: HI $PERSON2 how are you 
-$PERSON2: yeah fine $PERSON1 what about you where is $PERSON4
-$PERSON1: $PERSON4 is on the way 
-$PERSON1: all set for party?
-$PERSON2: yeah waiting for my $PERSON3
-$PERSON1: i think $PERSON4 gnna be full at today
-$PERSON2: Yeah!!!!!
+echo "$PERSON1: HI $PERSON2 how are you"
+echo "$PERSON2: yeah fine $PERSON1 what about you where is $PERSON4"
+echo "$PERSON1: $PERSON4 is on the way" 
+echo "$PERSON1: all set for party?"
+echo "$PERSON2: yeah waiting for my $PERSON3"
+echo "$PERSON1: i think $PERSON4 gnna be full at today"
+echo "$PERSON2: Yeah!!!!!"
