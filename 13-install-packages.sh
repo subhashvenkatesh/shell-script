4c9a030356275c326f07633cb6d046110378f839
yamaha <venkateshsubhash.sv@gmail.com>
2024-05-04 18:31:28 +0530

    installing Multiple packages

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -2,6 +2,14 @@
 
 ID=$(id -u)
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+    echo "$2...Failed"
+    else
+    echo "$2...Success"
+    fi
+}
 if [ $ID -ne 0 ]
 
 then 
@@ -10,4 +18,16 @@ else
     echo "Your a root user"
 fi
 
-echo "All args passed: $@"
+#echo "All args passed: $@"
+
+for package in $@
+do
+    yum installed $package
+    if [ $? -ne 0 ]
+    then
+        yum install $package -y
+        VALIDATE $? "Installation of $package"
+    else
+        echo "$package already installed.....skipping"
+    fi    
+done
