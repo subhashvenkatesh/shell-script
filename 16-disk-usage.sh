4b49b25010357f8f168cbe8122eccafcd0c6d88e
yamaha <venkateshsubhash.sv@gmail.com>
2024-05-11 20:53:26 +0530

    deleting old log files

--- 16-disk-usage.sh
+++ 16-disk-usage.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep -vE 'tmp|file')
-DISK_THRESHOLD=2
+DISK_THRESHOLD=1
 message=""
 
 while IFS= read line
 do
     usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
     partition=$(echo $line | awk '{print $1F}')
-    if [ $usage -gt $DISK_THRESHOLD ]
+    if [ $usage -ge $DISK_THRESHOLD ]
     then
         message+="High Disk Usage on $partition: $usage\n"
     fi    
