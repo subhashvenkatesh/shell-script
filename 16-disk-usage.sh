2c7914c19b1d1a2e901cb49fddcf2b7263f1a003
yamaha <venkateshsubhash.sv@gmail.com>
2024-05-11 22:46:18 +0530

    deleting old log files

--- 16-disk-usage.sh
+++ 16-disk-usage.sh
@@ -16,4 +16,6 @@ done <<< $DISK_USAGE
 
 echo -e "Message: $message"
 
-echo "$message" | mail -s "High Disk Usage" venkateshsubhash.sv@gmail.com
+#echo "$message" | mail -s "High Disk Usage" venkateshsubhash.sv@gmail.com
+
+sh mail.sh "DevOps Team" "High Disk Usage" "$message" "subhashind1947@gmail.com" "Alert High Disk Usage"
